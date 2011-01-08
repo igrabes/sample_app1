b82c113d7ae05c7ec0d4438e51e25d801fbfa242
Ian <ian.grabill@gmail.com>
2011-01-08 11:06:50 -0500

    User Model Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,14 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :name, :email
+  
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :name,  :presence => true, 
+                    :length   => { :maximum => 50 }
+  validates :email, :presence   => true,
+                    :format     => { :with => email_regex }, # notice the comma after the hash
+                    :uniqueness => { :case_sensitive => false }
+ 
 end
